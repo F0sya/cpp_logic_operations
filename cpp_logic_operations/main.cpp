49e7e177f97414b43cce961b675211c6e35cbf56
F0sya <nikitachmyria@gmail.com>
2024-04-25 20:11:01 +0300

    The third homework exercise is done

--- cpp_logic_operations/main.cpp
+++ cpp_logic_operations/main.cpp
@@ -6,10 +6,11 @@ using namespace std;
 
 int main() {
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-	float radius; cout << "Radius of sphere(cm):";  SetConsoleTextAttribute(hConsole, 0x05); cin >> radius; SetConsoleTextAttribute(hConsole, 0x0F);
-	cout << "Volume of sphere: ";
-	SetConsoleTextAttribute(hConsole, 0x05);
-	cout << (4 * M_PI * (radius*radius*radius)) / 3;
-	SetConsoleTextAttribute(hConsole, 0x0F);
-	cout << " cub.cm" << endl;
+	cout << "Calculation of running speed." << endl;
+	float length; cout << "Enter a length of distance(metres): ";  SetConsoleTextAttribute(hConsole, 0x05); cin >> length; SetConsoleTextAttribute(hConsole, 0x0F);
+	float time; cout << "Enter a time: ";  SetConsoleTextAttribute(hConsole, 0x05); cin >> time; SetConsoleTextAttribute(hConsole, 0x0F);
+	cout << "Distance: "; SetConsoleTextAttribute(hConsole, 0x05); cout << length; SetConsoleTextAttribute(hConsole, 0x0F); cout << " metres" << endl;
+	cout << "Time: "; SetConsoleTextAttribute(hConsole, 0x05); cout << static_cast<int>(time); SetConsoleTextAttribute(hConsole, 0x0F); cout << " m "; SetConsoleTextAttribute(hConsole, 0x05); cout << (time - (static_cast<int>(time))) * 100; SetConsoleTextAttribute(hConsole, 0x0F); cout << " sec = "; SetConsoleTextAttribute(hConsole, 0x05); cout << (static_cast<int>(time) * 60) + ((time - (static_cast<int>(time))) * 100); SetConsoleTextAttribute(hConsole, 0x0F); cout << " sec" << endl;
+	cout << "Your speed: "; SetConsoleTextAttribute(hConsole, 0x05); cout << (length / 1000) / (((static_cast<int>(time) * 60) + ((time - (static_cast<int>(time))) * 100)) / 3600); SetConsoleTextAttribute(hConsole, 0x0F); cout << " km per hours" << endl;
+
 }
