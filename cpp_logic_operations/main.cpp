430d9103e4f85b2808285d0e82743be724db13c3
F0sya <nikitachmyria@gmail.com>
2024-04-25 19:23:58 +0300

    The third practice exercise is done

---
+++ cpp_logic_operations/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main() {
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	float length; cout << "Length (cm): "; SetConsoleTextAttribute(hConsole, 0x05); cin >> length; SetConsoleTextAttribute(hConsole, 0x0F);
+	float width; cout << "Width (cm): "; SetConsoleTextAttribute(hConsole, 0x05); cin >> width; SetConsoleTextAttribute(hConsole, 0x0F);
+	float height; cout << "Height (cm): "; SetConsoleTextAttribute(hConsole, 0x05); cin >> height; SetConsoleTextAttribute(hConsole, 0x0F);
+	cout << "Volume: ";
+	SetConsoleTextAttribute(hConsole, 0x05);
+	cout << (length * width) * height;
+	SetConsoleTextAttribute(hConsole, 0x0F);
+	cout << " cub.m" << endl;
+}
