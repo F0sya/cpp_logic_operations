6caafa0c1f8efa36a3d18abc46578b795bffb3e8
F0sya <nikitachmyria@gmail.com>
2024-04-25 19:36:35 +0300

    fixed localization

--- cpp_logic_operations/main.cpp
+++ cpp_logic_operations/main.cpp
@@ -3,9 +3,7 @@
 using namespace std;
 
 int main() {
-	SetConsoleOutputCP(1251);
-	SetConsoleCP(1251);
-	int grn; cout << "Гривні:";  cin >> grn;
-	int kop; cout << "Копійки:"; cin >> kop;
-	cout << grn + ((kop - (kop % 100)) / 100) << " грн " << kop % 100 << " коп " << endl;
+	int grn; cout << "Hryvnia:";  cin >> grn;
+	int kop; cout << "Kopeks:"; cin >> kop;
+	cout << grn + ((kop - (kop % 100)) / 100) << " hrn " << kop % 100 << " kop " << endl;
 }
